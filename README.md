80afb86a3183e94e2a1a83e4207522776f4b66cf
Tulit <tulitpal@gmail.com>
2020-11-03 21:45:22 +0530

    Initial commit
    This is the initial commit and contains the basic files as well as a
    small HelloWorld program for Java

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Hackerrank-Solutions
+# HackerrankSolutions
+
+This repository contains code that I write while practicing various types of problems on Hackerrank
---
+++ src/main/java/practice_java/easy/WelcomeToJava/Solution.java
@@ -0,0 +1,15 @@
+package practice_java.easy.WelcomeToJava;
+
+/**
+ * This is a simple code that prints texts to two separate lines
+ *
+ * @author T
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+    }
+}
