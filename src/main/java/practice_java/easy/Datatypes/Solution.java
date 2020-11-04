d7768e89183e5b5501757c3ea02e0c1d47e5898c
Tulit <tulitpal@gmail.com>
2020-11-04 20:49:02 +0530

    Commit for Factory Pattern (#3)

--- src/main/java/practice_java/easy/Datatypes/Solution.java
+++ src/main/java/practice_java/easy/Datatypes/Solution.java
@@ -12,7 +12,7 @@ class Solution {
 
 	public static void main (String []args) {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
         for(int i = 0; i < t; i++) {
