83854bdf750a8a8739e67a8e066796f7789d8318
Tulit <tulitpal@gmail.com>
2020-11-04 19:56:29 +0530

    Commit for Java Datatypes (#2)

---
+++ src/main/java/practice_java/easy/Datatypes/Solution.java
@@ -0,0 +1,41 @@
+package practice_java.easy.Datatypes;
+
+import java.util.Scanner;
+
+/**
+ * This is a simple code that accepts integer and prints what all integer data-types can hold it.
+ *
+ * @author T
+ */
+
+class Solution {
+
+	public static void main (String []args) {
+
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if(x >= -128 && x <= 127)
+                System.out.println("* byte");
+
+                if(x >= ((-1)*Math.pow(2,15)) && x <= (Math.pow(2,15)-1))
+                System.out.println("* short");
+
+                if(x >= ((-1)*Math.pow(2,31)) && x <= (Math.pow(2,31)-1))
+                System.out.println("* int");
+
+                if(x >= ((-1)*Math.pow(2,63)) && x <= (Math.pow(2,63)-1))
+                System.out.println("* long");
+            }
+            catch(Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+
+        sc.close();
+    }
+}
